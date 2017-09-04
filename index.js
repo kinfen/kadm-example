857b758fa13d9deb8ff075a7b5bbd39cabe4fd81
Kinfen <kinfen.in@gmail.com>
2017-09-04 18:37:05 +0800

    调整环境变量

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ var kadm = require('./kadm');
 module.exports = exports = new function()
 {
 	
-	//process.env.CLOUDINARY_URL="cloudinary://247181424266945:cHUwYyEGRV7WH_8bdjTmkydGP7c@hwqie6qjg";
-	//process.env.COOKIE_SECRET="99422c0d14bdc450d124958ba098d3bed10e8bdc537781d654e7a4bdbddb20ca";
-	//process.env.EMAIL_HOSTNAME="";
-	//process.env.MANDRILL_APIKEY="MJBMvNWAZCSKUNHxHC1vAA";
-	//process.env.MANDRILL_USERNAME="app31974532@heroku.com";
-	//process.env.NODE_ENV="debug";
-	//process.env.PAPERTRAIL_API_TOKEN="yucq0bU4ls8XjzBzPQ2";
-	//process.env.MONGOLAB_URL="mongodb://localhost/ksitecore";
+	process.env.CLOUDINARY_URL="cloudinary://247181424266945:cHUwYyEGRV7WH_8bdjTmkydGP7c@hwqie6qjg";
+	process.env.COOKIE_SECRET="99422c0d14bdc450d124958ba098d3bed10e8bdc537781d654e7a4bdbddb20ca";
+	process.env.EMAIL_HOSTNAME="";
+	process.env.MANDRILL_APIKEY="MJBMvNWAZCSKUNHxHC1vAA";
+	process.env.MANDRILL_USERNAME="app31974532@heroku.com";
+	process.env.NODE_ENV="debug";
+	process.env.PAPERTRAIL_API_TOKEN="yucq0bU4ls8XjzBzPQ2";
+	process.env.MONGOLAB_URL="mongodb://mongo/kadm";
 
-	kadm.init();
+	kadm.init({
+		"port":	3000
+	});
 	kadm.set("routes", require("./routes"));
 	kadm.start();
 };
